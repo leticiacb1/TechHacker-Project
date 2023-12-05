4352d22aa263ab4b275c40e9f5a16febc8495a94
leticiacb1 <leticiacb1@al.insper.edu.br>
2023-12-04 21:07:02 -0300

    Fix Bugs.

---
+++ terraform/0-provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.67.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = var.AWS_REGION        # us-east-1
+  access_key = var.AWS_ACCESS_KEY_ID # Após obter acess key no Dashboard da Amazon
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+
+  default_tags {
+    tags = {
+      managed-by = "terraform"
+    }
+  }
+}
